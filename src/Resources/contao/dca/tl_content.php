8ccbbe972bd814be25ddfa5cd1b83819b45c9fff
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 11:42:33 -0400

    Create tl_content.php

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Hero Text
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ */
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['hero_text'] = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+
+?>
