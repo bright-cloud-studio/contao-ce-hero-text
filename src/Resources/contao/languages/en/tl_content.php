348e1b4e15c4ef8c365e87b1117f2514aad18114
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 12:24:52 -0400

    Create tl_content.php

---
+++ src/Resources/contao/languages/en/tl_content.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Hero Text
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['buttonLink'][0] = 'Button Link';
+$GLOBALS['TL_LANG']['tl_content']['buttonLink'][1] = 'Please choose the page to which our hero button will link to';
