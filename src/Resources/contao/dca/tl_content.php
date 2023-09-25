e4fe0b6e61ed33c0251e32a9617e65ed79e2315d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 12:23:10 -0400

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -8,5 +8,22 @@
  * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
  */
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['hero_text'] = '{type_legend},type,headline;{text_legend},text;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
-?>
+// Get our default 'tl_content' DCA
+$dc = &$GLOBALS['TL_DCA']['tl_content'];
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['hero_text'] = '{type_legend},type,headline;{text_legend},text,buttonLink;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
+
+$arrFields = array(
+    'buttonLink'                => array(
+        'label'      => &$GLOBALS['TL_LANG']['tl_content']['buttonLink'],
+        'exclude'    => true,
+        'inputType'  => 'pageTree',
+        'foreignKey' => 'tl_page.title',
+        'eval'       => array('mandatory' => false, 'fieldType' => 'radio'),
+        'sql'        => "int(10) unsigned NOT NULL default '0'",
+        'relation'   => array('type' => 'belongsTo', 'load' => 'lazy'),
+    )
+);
+
+$dc['fields'] = array_merge($dc['fields'], $arrFields);
+
