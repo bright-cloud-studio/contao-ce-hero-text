2b5d8eed5cc03606efd92b97482dbdbf241a61d0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 11:44:26 -0400

    Create CEHeroText.php

---
+++ src/Resources/contao/elements/CEHeroText.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Hero Text
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ */
+
+namespace Bcs\ContaoCEHeroTextBundle;
+
+
+class ContentHeroText extends \ContentElement
+{
+
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'ce_hero_text';
+
+
+	/**
+	 * Generate the content element
+	 */
+	protected function compile()
+	{
+		return;
+	}
+}
