708c5e90a5dbed818a8ee77ab9104283784a0683
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 11:28:58 -0400

    Create BcsContaoCEHeroTextBundle.php

---
+++ src/BcsContaoCEHeroTextBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of bright-cloud-studio/contao-ce-hero-text
+ *
+ * (c) 2023 Bright Cloud Studio
+ *
+ */
+
+namespace Bcs\ContaoCEHeroTextBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsContaoCEHeroTextBundle extends Bundle
+{
+}
