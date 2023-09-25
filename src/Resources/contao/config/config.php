a65eeddec4dcf78ead4138334c5298a263dd33d1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 11:49:25 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Hero Text
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ */
+
+ array_insert($GLOBALS['TL_CTE']['texts'], 10, array
+ (
+ 	'hero_text' => 'Bcs\HeroTextBundle\ContentHeroText')
+ );
+
+?>
