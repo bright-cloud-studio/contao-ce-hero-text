e29946f2972b1152608a6bd5fedebcaeeb8352ac
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 11:41:38 -0400

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Hero Text
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ */
+
+namespace Bcs\ContaoCEHeroText\ContaoManager;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Bcs\ContaoCEHeroText\BcsContaoCEHeroTextBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
+                ->setReplace(['divider']),
+        ];
+    }
+}
