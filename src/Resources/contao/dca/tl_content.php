06f98b3f20ff0d5e496c416c781765796f720559
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-25 12:14:12 -0400

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -8,5 +8,5 @@
  * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
  */
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['hero_text'] = '{type_legend},type,headline;{text_legend},text;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop',
+$GLOBALS['TL_DCA']['tl_content']['palettes']['hero_text'] = '{type_legend},type,headline;{text_legend},text;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
 ?>
