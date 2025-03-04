e7eccd95fc8ac65ee1134e18db2bbd8cb38df71c
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-04 12:53:25 -0500

    Update ContentHeroText.php

--- src/Resources/contao/elements/ContentHeroText.php
+++ src/Resources/contao/elements/ContentHeroText.php
@@ -10,21 +10,43 @@
 
 namespace Bcs\HeroTextBundle;
 
-class ContentHeroText extends \ContentText
+class ContentText extends \ContentElement
 {
-
 	/**
 	 * Template
 	 * @var string
 	 */
 	protected $strTemplate = 'ce_hero_text';
 
-
 	/**
 	 * Generate the content element
 	 */
-	public function compile()
+	protected function compile()
 	{
-		parent::compile();
+		// Add the static files URL to images
+		if ($staticUrl = System::getContainer()->get('contao.assets.files_context')->getStaticUrl())
+		{
+			$path = System::getContainer()->getParameter('contao.upload_path') . '/';
+			$this->text = str_replace(' src="' . $path, ' src="' . $staticUrl . $path, (string) $this->text);
+		}
+
+		$this->Template->text = StringUtil::encodeEmail((string) $this->text);
+		$this->Template->addImage = false;
+		$this->Template->addBefore = false;
+
+		// Add an image
+		if ($this->addImage)
+		{
+			$figure = System::getContainer()
+				->get('contao.image.studio')
+				->createFigureBuilder()
+				->from($this->singleSRC)
+				->setSize($this->size)
+				->setOverwriteMetadata($this->objModel->getOverwriteMetadata())
+				->enableLightbox($this->fullsize)
+				->buildIfResourceExists();
+
+			$figure?->applyLegacyTemplateData($this->Template, null, $this->floating);
+		}
 	}
 }
