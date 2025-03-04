249234ce3bb129648f03e9cc161181de4bbb7df8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-04 14:06:13 -0500

    Update _runonce.php

--- src/Resources/contao/config/_runonce.php
+++ src/Resources/contao/config/_runonce.php
@@ -3,17 +3,21 @@ class HeroTextRunonceJobTemp extends Controller
 {
     public function __construct()
     {
-        parent::__construct();
-        $this->import('Files');
+        //parent::__construct();
+        //$this->import('Files');
     }
     public function run()
     {
+        /*
         if ((@include(TL_ROOT . '/system/modules/HeroText/config/config.php')) !== false)
         {
             $this->Files->rrdir('system/modules/HeroText');
         }
+        */
     }
 }
-$objHeroTextRunonceJob = new HeroTextRunonceJobTemp();
-$objHeroTextRunonceJob->run();
+
+//$objHeroTextRunonceJob = new HeroTextRunonceJobTemp();
+//$objHeroTextRunonceJob->run();
+
 ?>
